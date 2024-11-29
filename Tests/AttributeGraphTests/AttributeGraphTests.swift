5e150d0f592ec47ce7c55da1d0263bb3fa8b02e8
Daniel Tull <dt@danieltull.co.uk>
2024-11-29 21:02:16 +0000

    Add a test where a value e continues to be marked as pending because it hasn't been accessed yet

--- Tests/AttributeGraphTests/AttributeGraphTests.swift
+++ Tests/AttributeGraphTests/AttributeGraphTests.swift
@@ -69,7 +69,7 @@ import Testing
 
   #expect(d.wrappedValue == 120)
 
-  let str3 = """
+  #expect(graph.graphViz == """
     digraph {
     A
     B
@@ -79,6 +79,79 @@ import Testing
     B -> C
     C -> D
     }
-    """
-  #expect(str3 == graph.graphViz)
+    """)
+}
+
+@Test func example3() async throws {
+  let graph = AttributeGraph()
+  let a = graph.input(name: "A", 10)
+  let b = graph.input(name: "B", 20)
+  let c = graph.rule(name: "C") { a.wrappedValue + b.wrappedValue }
+  let d = graph.rule(name: "D") { c.wrappedValue * 2 }
+  let e = graph.rule(name: "E") { a.wrappedValue * 2 }
+  #expect(c.wrappedValue == 30)
+  #expect(d.wrappedValue == 60)
+  #expect(e.wrappedValue == 20)
+
+  #expect(graph.graphViz == """
+    digraph {
+    A
+    B
+    C
+    D
+    E
+    A -> C
+    A -> E
+    B -> C
+    C -> D
+    }
+    """)
+
+  a.wrappedValue = 40
+
+  #expect(graph.graphViz == """
+    digraph {
+    A
+    B
+    C [style=dashed]
+    D [style=dashed]
+    E [style=dashed]
+    A -> C [style=dashed]
+    A -> E [style=dashed]
+    B -> C
+    C -> D
+    }
+    """)
+
+  #expect(d.wrappedValue == 120)
+
+  #expect(graph.graphViz == """
+    digraph {
+    A
+    B
+    C
+    D
+    E [style=dashed]
+    A -> E [style=dashed]
+    A -> C
+    B -> C
+    C -> D
+    }
+    """)
+
+  #expect(e.wrappedValue == 80)
+
+  #expect(graph.graphViz == """
+    digraph {
+    A
+    B
+    C
+    D
+    E
+    A -> C
+    A -> E
+    B -> C
+    C -> D
+    }
+    """)
 }
