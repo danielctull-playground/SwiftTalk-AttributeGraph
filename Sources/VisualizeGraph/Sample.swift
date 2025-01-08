5bbef6dac62ad8efcb4734a6e4276d3c147b36c1
Daniel Tull <dt@danieltull.co.uk>
2025-01-08 18:02:49 +0000

    Improve sample

--- Sources/VisualizeGraph/Sample.swift
+++ Sources/VisualizeGraph/Sample.swift
@@ -10,12 +10,32 @@ struct Sample: View {
     VStack {
       if index >= 0, index < snapshots.count {
         Graphviz(dot: snapshots[index].dot)
+          .frame(maxWidth: .infinity, maxHeight: .infinity)
+        Stepper(value: $index) {
+          Text("step \(index + 1) / \(snapshots.count)")
+        }
+        .padding()
       }
     }
     .onAppear {
       let graph = AttributeGraph()
-      let a = graph.input(name: "A", 100)
-      graph.rule(name: "B") { a.wrappedValue * 2 }
+      let a = graph.input(name: "A", 10)
+      let b = graph.input(name: "B", 20)
+      let c = graph.rule(name: "C") { a.wrappedValue + b.wrappedValue }
+      let d = graph.rule(name: "D") { c.wrappedValue * 2 }
+      let e = graph.rule(name: "E") { a.wrappedValue * 2 }
+      snapshots.append(Snapshot(graph: graph))
+      _ = e.wrappedValue
+      snapshots.append(Snapshot(graph: graph))
+      _ = d.wrappedValue
+      snapshots.append(Snapshot(graph: graph))
+      b.wrappedValue += 1
+      snapshots.append(Snapshot(graph: graph))
+      a.wrappedValue += 1
+      snapshots.append(Snapshot(graph: graph))
+      _ = d.wrappedValue
+      snapshots.append(Snapshot(graph: graph))
+      _ = e.wrappedValue
       snapshots.append(Snapshot(graph: graph))
     }
   }
