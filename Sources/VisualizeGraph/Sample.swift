b751cccfbbb6a2f344fff85849dacb875efbe913
Daniel Tull <dt@danieltull.co.uk>
2025-01-09 09:55:31 +0000

    Write an example of a LayoutComputer

--- Sources/VisualizeGraph/Sample.swift
+++ Sources/VisualizeGraph/Sample.swift
@@ -18,7 +18,7 @@ struct Sample: View {
       }
     }
     .onAppear {
-      snapshots = hstack()
+      snapshots = layout()
     }
   }
 }
@@ -57,6 +57,80 @@ func hstack() -> [Snapshot] {
   snapshots.append(Snapshot(graph: graph))
 
   let _ = hstack.wrappedValue
+  snapshots.append(Snapshot(graph: graph))
+
+  return snapshots
+}
+
+struct LayoutComputer {
+  let sizeThatFits: (ProposedViewSize) -> CGSize
+}
+
+func layout() -> [Snapshot] {
+
+  var snapshots: [Snapshot] = []
+
+  let graph = AttributeGraph()
+  let toggle = graph.input(name: "toggle", false)
+  let proposal = graph.input(name: "size", ProposedViewSize(width: 200, height: 100))
+
+  let redLayoutComputer = graph.rule(name: "red layout computer") {
+    LayoutComputer { $0.replacingUnspecifiedDimensions() }
+  }
+
+  let nestedLayoutComputer = graph.rule(name: "nested layout computer") {
+    let toggle = toggle.wrappedValue
+    return LayoutComputer { proposal in
+      CGSize(
+        width: toggle ? 50 : 100,
+        height: proposal.height ?? 10
+      )
+    }
+  }
+
+  let hstackLayoutComputer = graph.rule(name: "hstack layout computer") {
+
+    let nestedLayoutComputer = nestedLayoutComputer.wrappedValue
+    let redLayoutComputer = redLayoutComputer.wrappedValue
+
+    return LayoutComputer { proposal in
+      var remainder = proposal.width!
+
+      let nestedSize = nestedLayoutComputer
+        .sizeThatFits(ProposedViewSize(width: remainder/2, height: proposal.height))
+      remainder -= nestedSize.width
+
+      let redSize = redLayoutComputer
+        .sizeThatFits(ProposedViewSize(width: remainder, height: proposal.height))
+
+      let result = CGSize(
+        width: redSize.width + nestedSize.width,
+        height: max(redSize.height, nestedSize.height)
+      )
+
+      return result
+    }
+  }
+
+  let size = graph.rule(name: "hstack size") {
+    hstackLayoutComputer.wrappedValue.sizeThatFits(proposal.wrappedValue)
+  }
+
+  snapshots.append(Snapshot(graph: graph))
+
+  _ = size.wrappedValue
+  snapshots.append(Snapshot(graph: graph))
+
+  toggle.wrappedValue.toggle()
+  snapshots.append(Snapshot(graph: graph))
+
+  _ = size.wrappedValue
+  snapshots.append(Snapshot(graph: graph))
+
+  proposal.wrappedValue.width = 300
+  snapshots.append(Snapshot(graph: graph))
+
+  _ = size.wrappedValue
   snapshots.append(Snapshot(graph: graph))
 
   return snapshots
